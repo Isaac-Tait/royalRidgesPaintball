9595b876e7fff69f042cbb18346ce431f06c91ec
Isaac Tait <isaac@mountaintopcoding.com>
2023-06-29 19:34:14 -0400

    chore: larger screen responsive design 🕹️

--- src/app/page.js
+++ src/app/page.js
@@ -4,30 +4,34 @@ import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <div className='flex flex-col h-screen bg-slate-200'>
+    <div className='flex flex-col h-screen bg-slate-200 overflow-y-scroll'>
       {/* Header */}
       <div>
-        <p className='text-center font-semibold text-3xl py-2 font-sans text-yellow-500'>
+        <p className='font-semibold text-3xl md:text-5xl md:text-center font-sans text-yellow-500 md:pb-4'>
           Royal Ridges Paintball&nbsp;
         </p>
       </div>
       {/* Body */}
-      <div className='md:heropattern-bubbles-gray-400'>
-        <div className=''>
+      <div className='flex-grow md:heropattern-bubbles-gray-400'>
+        <div className='flex justify-center mt-1 md:mt-4'>
           <Image
             src='https://res.cloudinary.com/mountaintopcoding-127956/image/upload/v1688049329/royalRidges/paintball_ajydhr.jpg'
             alt='paintball warrior doctored image'
             width={700}
             height={900}
+            placeholder='blur'
+            blurDataURL='https://image-component.nextjs.gallery/placeholder'
+            className='md:rounded-xl'
           />
         </div>
+        
+          <h3 className='text-center font-serif text-slate-800 text-base md:text-2xl md:pt-8'>
+            The best location for paintball action in the Battle
+            Ground/Vancouver Washington area
+          </h3>
+        
       </div>
-      <div className='flex-grow'>
-        <h3 className='text-center font-serif text-slate-800'>
-          The best location for paintball action in the Battle
-          Ground/Vancouver Washington area
-        </h3>
-      </div>
+
       {/* Footer */}
       <div className='fixed bottom-0 w-full'>
         <Footer />
