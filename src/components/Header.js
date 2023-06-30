463e8bc321f6aeeb836b41bbdea3b60470365e88
Isaac Tait <isaac@mountaintopcoding.com>
2023-06-30 11:22:35 -0400

    chore: tackling blog styling take 1 🎬

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,13 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <div>
-      <div className='w-full flex justify-between bg-blue-900'>
-        <h1 className='text-6xl font-semibold text-red-400 py-4 ml-2'>
+      <div className='w-full flex justify-between bg-green-500'>
+        <h1 className='text-6xl font-semibold text-yellow-400 py-4 ml-2'>
           Royal Ridges Paintball
         </h1>
       </div>
       <div className=''>
-        <ul className='flex justify-between bg-blue-400'>
+        <ul className='flex justify-between bg-green-500'>
           <Link className='text-red-400' href='/blog'>
             Blog
           </Link>
--- src/pages/blog/[filename].js
+++ src/pages/blog/[filename].js
@@ -2,6 +2,9 @@ import { useTina } from 'tinacms/dist/react';
 import { TinaMarkdown } from 'tinacms/dist/rich-text';
 import client from '../../../tina/__generated__/client';
 
+import Link from 'next/link';
+import Head from 'next/head';
+
 import Header from '../../components/Header';
 
 const BlogPage = (props) => {
@@ -13,28 +16,29 @@ const BlogPage = (props) => {
 
   return (
     <>
-      <div>
+      <Head>
+        {/* Tailwind CDN */}
+        
+      </Head>
+      <div className='heropattern-bubbles-green-500'>
         <Header />
         <div
           style={{
             textAlign: 'center',
           }}
         >
-          <h1 className='text-3xl m-8 text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
+          <h1 className='text-3xl m-8 text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl font-sans'>
             {data.post.title}
           </h1>
           <ContentSection content={data.post.body}></ContentSection>
         </div>
         <div className='bg-green-100 text-center'>
-          Lost and looking for a place to start?
-          <a
-            href='https://tina.io/guides/tina-cloud/getting-started/overview/'
-            className='text-yellow-500 underline'
-          >
+          Royal Ridges&apos; Paintball has been around since 2010 -
+          <Link href='/about' className='text-yellow-500 underline'>
             {' '}
-            Check out this guide
-          </a>{' '}
-          to see how add TinaCMS to an existing Next.js site.
+            learn more at our About page
+          </Link>{' '}
+          and see you on the field!
         </div>
       </div>
     </>
@@ -93,109 +97,6 @@ const components = {
 const ContentSection = ({ content }) => {
   return (
     <div className='relative py-16 bg-white overflow-hidden'>
-      <div className='hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full'>
-        <div
-          className='relative h-full text-lg max-w-prose mx-auto'
-          aria-hidden='true'
-        >
-          <svg
-            className='absolute top-12 left-full transform translate-x-32'
-            width={404}
-            height={384}
-            fill='none'
-            viewBox='0 0 404 384'
-          >
-            <defs>
-              <pattern
-                id='74b3fd99-0a6f-4271-bef2-e80eeafdf357'
-                x={0}
-                y={0}
-                width={20}
-                height={20}
-                patternUnits='userSpaceOnUse'
-              >
-                <rect
-                  x={0}
-                  y={0}
-                  width={4}
-                  height={4}
-                  className='text-gray-200'
-                  fill='currentColor'
-                />
-              </pattern>
-            </defs>
-            <rect
-              width={404}
-              height={384}
-              fill='url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)'
-            />
-          </svg>
-          <svg
-            className='absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32'
-            width={404}
-            height={384}
-            fill='none'
-            viewBox='0 0 404 384'
-          >
-            <defs>
-              <pattern
-                id='f210dbf6-a58d-4871-961e-36d5016a0f49'
-                x={0}
-                y={0}
-                width={20}
-                height={20}
-                patternUnits='userSpaceOnUse'
-              >
-                <rect
-                  x={0}
-                  y={0}
-                  width={4}
-                  height={4}
-                  className='text-gray-200'
-                  fill='currentColor'
-                />
-              </pattern>
-            </defs>
-            <rect
-              width={404}
-              height={384}
-              fill='url(#f210dbf6-a58d-4871-961e-36d5016a0f49)'
-            />
-          </svg>
-          <svg
-            className='absolute bottom-12 left-full transform translate-x-32'
-            width={404}
-            height={384}
-            fill='none'
-            viewBox='0 0 404 384'
-          >
-            <defs>
-              <pattern
-                id='d3eb07ae-5182-43e6-857d-35c643af9034'
-                x={0}
-                y={0}
-                width={20}
-                height={20}
-                patternUnits='userSpaceOnUse'
-              >
-                <rect
-                  x={0}
-                  y={0}
-                  width={4}
-                  height={4}
-                  className='text-gray-200'
-                  fill='currentColor'
-                />
-              </pattern>
-            </defs>
-            <rect
-              width={404}
-              height={384}
-              fill='url(#d3eb07ae-5182-43e6-857d-35c643af9034)'
-            />
-          </svg>
-        </div>
-      </div>
       <div className='relative px-4 sm:px-6 lg:px-8'>
         <div className='text-lg max-w-prose mx-auto'>
           <TinaMarkdown components={components} content={content} />
